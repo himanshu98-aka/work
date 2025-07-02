37b7423e0e1306133b1e12aeb5cd720cc2f81336
himanshu98-aka <freelancinghimanshu@gmail.com>
2025-07-02 14:56:14 +0530

    Updated sum_calculator.c

--- day 1/sum_calculator.c
+++ day 1/sum_calculator.c
@@ -2,14 +2,14 @@
 int main(){
     int num1,num2,sum;
     printf("welcome to sum calculator\n");
-    printf("enter numaber 1 for addition :");
-    scanf("%d",&num1);
+    printf("enter number 1 for addition :");
+    scanf("%d",&num1); // remember to use & this to input value in variable
 
-    printf("enter numaber 2 for addition");
+    printf("enter number 2 for addition :");
     scanf("%d",&num2);
 
     sum= num1 + num2 ;
-    printf("number 1 + number 2 =%d",sum);
+    printf("addition of number 1 + number 2 =%d",sum);
 
     return 0;   
 }
